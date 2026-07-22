90f3f27ac92141745242fc9fc7443476372bd355
Em Karisch <emii.khaos@gmail.com>
2026-07-22 12:11:29 +0200

    refactor: extract update subgroup into lib

---
+++ data-updates.lua
@@ -0,0 +1,11 @@
+local lib = require("__khaostransportgroup__.prototypes.lib")
+
+lib.update_subgroup("locomotive", "item-with-entity-data", "locomotive", "vehicles-railway")
+lib.update_subgroup("cargo-wagon", "item-with-entity-data", "cargo-wagon", "vehicles-railway")
+lib.update_subgroup("fluid-wagon", "item-with-entity-data", "fluid-wagon", "vehicles-railway")
+lib.update_subgroup("artillery-wagon", "item-with-entity-data", "artillery-wagon", "vehicles-railway")
+
+lib.update_subgroup("car", "item-with-entity-data", "car", "vehicles-civilian")
+
+lib.update_subgroup("car", "item-with-entity-data", "tank", "vehicles-military")
+lib.update_subgroup("spider-vehicle", "item-with-entity-data", "spidertron", "vehicles-military")
