1f94f8722989a03ca1e1b50c6c6991fe20fcb94e
Em Karisch <emii.khaos@gmail.com>
2026-07-22 12:46:57 +0200

    feat: support AAI vehicles: ironclad

---
+++ prototypes/compat/aai-vehicles-ironclad-final-fixes.lua
@@ -0,0 +1,5 @@
+local lib = require("__khaostransportgroup__.prototypes.lib")
+
+if mods["aai-vehicles-ironclad"] then
+  lib.update_subgroup("car", "item-with-entity-data", "ironclad", "vehicles-waterway")
+end
