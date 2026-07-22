3cfa77620762ce304606c62265b817e1dfe60956
Em Karisch <emii.khaos@gmail.com>
2026-07-22 12:11:29 +0200

    refactor: extract update subgroup into lib

--- data.lua
+++ data.lua
@@ -1,6 +1,3 @@
-local khaoslib_entity = require("__khaoslib__.prototypes.entity")
-local khaoslib_item = require("__khaoslib__.prototypes.item")
-local khaoslib_recipe = require("__khaoslib__.prototypes.recipe")
 local khaoslib_item_group = require("__khaoslib__.prototypes.item-group")
 local khaoslib_item_subgroup = require("__khaoslib__.prototypes.item-subgroup")
 
@@ -23,23 +20,3 @@ khaoslib_item_subgroup:load {type = "item-subgroup", name = "vehicles-military",
 khaoslib_item_subgroup:load {type = "item-subgroup", name = "vehicle-equipment", group = "transport", order = "f[vehicle-equipment]"} :commit()
 khaoslib_item_subgroup:load {type = "item-subgroup", name = "vehicle-equipment-military", group = "transport", order = "fa[vehicle-equipment-military]"} :commit()
 khaoslib_item_subgroup:load {type = "item-subgroup", name = "vehicle-equipment-defense", group = "transport", order = "fb[vehicle-equipment-defense]"} :commit()
-
---- @param entity_type string
---- @param item_type khaoslib_item.Types
---- @param name string
---- @param subgroup data.ItemSubGroupID
-local function update_subgroup(entity_type, item_type, name, subgroup)
-  khaoslib_entity:load(entity_type, name):set {subgroup = subgroup} :commit()
-  khaoslib_item:load(item_type, name):set {subgroup = subgroup} :commit()
-  khaoslib_recipe:load(name):set {subgroup = subgroup} :commit()
-end
-
-update_subgroup("locomotive", "item-with-entity-data", "locomotive", "vehicles-railway")
-update_subgroup("cargo-wagon", "item-with-entity-data", "cargo-wagon", "vehicles-railway")
-update_subgroup("fluid-wagon", "item-with-entity-data", "fluid-wagon", "vehicles-railway")
-update_subgroup("artillery-wagon", "item-with-entity-data", "artillery-wagon", "vehicles-railway")
-
-update_subgroup("car", "item-with-entity-data", "car", "vehicles-civilian")
-
-update_subgroup("car", "item-with-entity-data", "tank", "vehicles-military")
-update_subgroup("spider-vehicle", "item-with-entity-data", "spidertron", "vehicles-military")
---
+++ prototypes/lib.lua
@@ -0,0 +1,18 @@
+local khaoslib_entity = require("__khaoslib__.prototypes.entity")
+local khaoslib_item = require("__khaoslib__.prototypes.item")
+local khaoslib_recipe = require("__khaoslib__.prototypes.recipe")
+
+--- @class KhaosTransportGroup.lib
+local lib = {}
+
+--- @param entity_type string
+--- @param item_type khaoslib_item.Types
+--- @param name string
+--- @param subgroup data.ItemSubGroupID
+function lib.update_subgroup(entity_type, item_type, name, subgroup)
+  khaoslib_entity:load(entity_type, name):set {subgroup = subgroup} :commit()
+  khaoslib_item:load(item_type, name):set {subgroup = subgroup} :commit()
+  khaoslib_recipe:load(name):set {subgroup = subgroup} :commit()
+end
+
+return lib
