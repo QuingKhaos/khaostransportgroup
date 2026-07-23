4f078f16ea785b7dd2eb3248c6093ad1a8795e74
Em Karisch <emii.khaos@gmail.com>
2026-07-23 06:56:09 +0200

    dev: simplify mods dir

--- .dev/factorio_2.0.ps1
+++ .dev/factorio_2.0.ps1
@@ -2,12 +2,12 @@ Set-Variable -Name workspace -Value $Env:FACTORIO_WORKSPACE
 Set-Variable -Name mod_base -Value $Env:FACTORIO_20_MODS_BASE
 Set-Variable -Name package -Value "khaostransportgroup"
 
-New-Item -ItemType Directory -Force -Path "$mod_base\$package" | Out-Null
+New-Item -ItemType Directory -Force -Path "$mod_base" | Out-Null
 
 Set-Variable -Name version -Value (Get-Content "$workspace\$package\info.json" | ConvertFrom-Json).version
-Move-Item -Path "$workspace\$package\${package}_${version}.zip" -Destination "${mod_base}\${package}" -Force -ErrorAction SilentlyContinue
+Move-Item -Path "$workspace\$package\${package}_${version}.zip" -Destination "$mod_base" -Force -ErrorAction SilentlyContinue
 
 Set-Variable -Name version_khaoslib -Value (Get-Content "$workspace\khaoslib\info.json" | ConvertFrom-Json).version
-Move-Item -Path "$workspace\khaoslib\khaoslib_$version_khaoslib.zip" -Destination "${mod_base}\${package}" -Force -ErrorAction SilentlyContinue
+Move-Item -Path "$workspace\khaoslib\khaoslib_$version_khaoslib.zip" -Destination "$mod_base" -Force -ErrorAction SilentlyContinue
 
-Start-Process -FilePath "${Env:FACTORIO_20_PATH}\bin\x64\Factorio.exe" -ArgumentList "--mod-directory ${mod_base}\${package}"
+Start-Process -FilePath "${Env:FACTORIO_20_PATH}\bin\x64\Factorio.exe" -ArgumentList "--mod-directory $mod_base"
